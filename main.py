e54d6c9a4e25b55a7f0ab507e03a1f29f0e30f13
Mingi-H <mingihon@buffalo.edu>
2022-11-05 18:44:27 -0400

    spawned player
    I spawned in the player!

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-xcoord = 0
-ycoord = 4
+column = 0
+row = 3
 
 import csv
 
 def fieldwrite(filename):
-    with open(filename,"w") as f:
+    with open(filename,"w",newline = "") as f:
         writer = csv.writer(f)
         for x in range(0,7):
             templist = []
             for k in range(0,81):
                 if(x%2==0):
-                    templist.append("_")
+                    templist.append("-")
                 if(x%2==1):
                     templist.append(" ")
             writer.writerow(templist)
 
 fieldwrite("field.csv")
 
-'''def setplayer(filename):
+def setplayer(filename):
     with open(filename) as f:
-        with open(filename,"w") as w:
-            reader = csv.reader(f)
-            count = 0
-            for line in reader:
-                if(ycoord = count):
-                    line[ycoord].append
-                count+=1 
+        reader = csv.reader(f)
+        out = []
+        count = 0
+        for line in reader:
+            if(row == count):
+                temp = line[column]
+                line.remove(temp)
+                line.insert(0,"P")
+            count += 1
+            out.append(line)
+    with open(filename,"w",newline = "") as w:
+        writer = csv.writer(w)
+        for list in out:
+            writer.writerow(list)
 
-        
-setplayer("field.csv")'''
+setplayer("field.csv")
+
 
 def setfield(filename):
     with open (filename) as f:
