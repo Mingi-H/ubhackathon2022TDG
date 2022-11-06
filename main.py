77fc9467b3cd96a47de85ad00a20362839e0b2c5
Mingi-H <mingihon@buffalo.edu>
2022-11-06 01:57:57 -0400

    made a functioning shoot function, as well as updating the enemy health/position.
    title

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ row = 3
 import random
 import csv
 
-enemytotal = []
+player_dmg = 3
 
 def fieldwrite(filename):
     with open(filename,"w",newline = "") as f:
@@ -13,9 +13,9 @@ def fieldwrite(filename):
             templist = []
             for k in range(0,81):
                 if(x%2==0):
-                    if(k%2 == 0):
+                    if(k%4 == 0):
                         templist.append("-")
-                    if(k%2 == 1):
+                    else:
                         templist.append(" ")
                 if(x%2==1):
                     templist.append(" ")
@@ -41,6 +41,19 @@ def setplayer(filename):
         for list in out:
             writer.writerow(list)
 
+def enemyregister(enemylist,xcoord,ycoord):
+    out = []
+    with open("enemydictionary.csv") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            for enemy in enemylist:
+                if(enemy == line[1]):
+                    out.append([line[1],line[2],line[3],xcoord,ycoord])
+    with open ("enemy.csv","w",newline = "") as f:
+        writer = csv.writer(f)
+        for list in out:
+            writer.writerow(list)
+
 def spawnenemy(filename, enemylist):
     with open(filename) as f:
         reader = csv.reader(f)
@@ -57,6 +70,7 @@ def spawnenemy(filename, enemylist):
                         if spawnlocation[enemy] == x:
                             line.pop()
                             line.append(enemy)
+                            enemyregister(enemylist,spawnlocation[enemy],line.index(enemy))
             count += 1
             out.append(line)
     with open(filename,"w",newline = "") as w:
@@ -64,14 +78,28 @@ def spawnenemy(filename, enemylist):
         for list in out:
             writer.writerow(list)
     
-def enemyregister(enemylist):
-    out = []
-    with open("enemydictionary.csv") as f:
-        reader = csv.reader(f)
-        for line in reader:
-            for enemy in enemylist:
-                if(enemy == line[1]):
-                    out.append([line[1],line[2],line[3]])
+def updateenemyposition():
+    with open("enemy.csv") as e:
+        out = []
+        readerenemy = csv.reader(e)
+        for line in readerenemy:
+            line[4] = int(line[4]) - int(line[2])
+            out.append(line)
+    with open ("enemy.csv","w",newline = "") as f:
+        writer = csv.writer(f)
+        for list in out:
+            writer.writerow(list)
+
+def updateenemyhealth(row):
+    with open("enemy.csv") as e:
+        out = []
+        readerenemy = csv.reader(e)
+        count = 0
+        for line in readerenemy:
+            if(count == row):
+                line[1] = int(line[1]) - player_dmg
+            out.append(line)
+            count += 1
     with open ("enemy.csv","w",newline = "") as f:
         writer = csv.writer(f)
         for list in out:
@@ -89,13 +117,36 @@ def enemywalk():
                         temp = line.index(lineenemy[0])
                         line.remove(lineenemy[0])
                         line.insert(temp, " ")
-                        line.remove(line[temp - 2])
-                        line.insert(temp-2,lineenemy[0])
+                        line.remove(line[temp - int(lineenemy[2])])
+                        line.insert(temp-int(lineenemy[2]),lineenemy[0])
                     out.append(line)
     with open("field.csv","w",newline = "") as w:
         writer = csv.writer(w)
         for list in out:
             writer.writerow(list)
+    updateenemyposition()
+
+def searchenemy():
+    with open("field.csv") as f:
+        with open("enemy.csv") as e:
+            reader = csv.reader(f)
+            readerenemy = csv.reader(e)
+            for lineenemy in readerenemy:
+                count = 0
+                for line in reader:
+                    if(lineenemy[0] in line):
+                        updateenemyhealth(count)
+                
+def shoot():
+    with open("field.csv") as f:
+        reader = csv.reader(f)
+        count = 0
+        for line in reader:
+            if(row == count):
+                print("Bang!")
+                searchenemy()
+            count += 1
+
 
 def setfield(filename):
     with open (filename) as f:
@@ -109,12 +160,12 @@ def setfield(filename):
 fieldwrite("field.csv")
 setplayer("field.csv")
 spawnenemy("field.csv",["BR"])
-enemyregister(["BR"])
 setfield("field.csv")
 
-print("Welcome to one of the tower defense games(?) of all time! To review the rules and controls, please type rules. This game was designed around a big enough console screen, so if the field looks wacky, extend the console by a bit and then reload the program. If you understand the rules or want to skip them, just start playing. Also, type stop to stop the game.")
+print("Welcome to one of the tower defense games of all time! To review the rules and controls, please type rules. This game was designed around a big enough console screen, so if the field looks wacky, extend the console by a bit and then reload the program. If you understand the rules or want to skip them, just start playing. Also, type stop to stop the game.")
 
 forever = True
+turn = 0
 while forever == True:
     text = input()
     if text == "stop":
@@ -139,5 +190,12 @@ while forever == True:
                 print(line)
     elif text == "":
         enemywalk()
+        turn += 1
+    elif text == "shoot":
+        shoot()
+        enemywalk()
+        turn += 1
+    print("it is currently turn " + str(turn) + " out of 50.")
     setfield("field.csv")
+    
 
