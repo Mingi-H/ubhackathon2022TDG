cae5897c173b27fdb13204071396a93e69fe15e5
Mingi-H <mingihon@buffalo.edu>
2022-11-06 04:02:06 -0500

    i think im about to do something stupid

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 column = 0
 row = 3
-
+player_dmg = 3
 import random
 import csv
 import os
 
+#This line of code exists to just reset the enemy file every time a new run starts. Otherwise, it keeps the enemies from last game and things get really weird.
 with open("enemy.csv","w",newline = "") as f:
     temp = 3
 
-player_dmg = 3
+#This code creates the "field", or basically the battle arena where the player has to shoot down the robots.
 
 def fieldwrite(filename):
     with open(filename,"w",newline = "") as f:
@@ -24,6 +25,8 @@ def fieldwrite(filename):
                 if(x%2==1):
                     templist.append(" ")
             writer.writerow(templist)
+
+#this code creates the player. Basically, it copies the field that is active right now, adds a P to the left, and then overwrites the field.
 
 def setplayer(filename):
     with open(filename) as f:
@@ -45,19 +48,7 @@ def setplayer(filename):
         for list in out:
             writer.writerow(list)
 
-def enemyregister(enemylist,xcoord,ycoord):
-    out = []
-    with open("enemydictionary.csv") as f:
-        reader = csv.reader(f)
-        for line in reader:
-            for enemy in enemylist:
-                if(enemy == line[1]):
-                    out.append([line[1],line[2],line[3],xcoord,ycoord])
-    with open ("enemy.csv","a",newline = "") as f:
-        writer = csv.writer(f)
-        for list in out:
-            writer.writerow(list)
-    
+#This code spawns the enemy, which basically much like creating the player, copies the field.csv and then puts an enemy on the far right.
 
 def spawnenemy(filename, enemylist):
     with open(filename) as f:
@@ -75,11 +66,56 @@ def spawnenemy(filename, enemylist):
                         if spawnlocation[enemy] == x:
                             line.pop()
                             line.append(enemy)
-                            enemyregister(enemylist,spawnlocation[enemy],line.index(enemy))
+                            enemyregister(enemylist,spawnlocation[enemy],80)
             count += 1
             out.append(line)
     with open(filename,"w",newline = "") as w:
         writer = csv.writer(w)
+        for list in out:
+            writer.writerow(list)
+
+#What this code does is it basically puts the enemy into an enemy.csv file, so that we can now keep track of which enemies have what health, are on what location, etc.
+
+def enemyregister(enemylist,y,x):
+    out = []
+    with open("enemydictionary.csv") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            for enemy in enemylist:
+                if(enemy == line[1]):
+                    out.append([line[1],line[2],line[3],y,x])
+    with open ("enemy.csv","a",newline = "") as f:
+        writer = csv.writer(f)
+        for list in out:
+            writer.writerow(list)
+
+#This code looks at what is currently in field.csv, and then copies it to the console to make it readable for the player.
+
+def setfield(filename):
+    with open (filename) as f:
+        reader = csv.reader(f)
+        for line in reader:
+            temp = ""
+            for index in line:
+                temp += index
+            print(temp)
+
+def fieldupdate():
+    out = []
+    with open("field.csv") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            out.append(line)
+    for list in out:
+        for index in range(len(list)):
+            if list[index] == "R":
+                list[index]= " "
+    with open("enemy.csv") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            out[int(line[3])][int(line[4])] = line[0]
+    with open ("field.csv","w",newline = "") as f:
+        writer = csv.writer(f)
         for list in out:
             writer.writerow(list)
     
@@ -129,6 +165,7 @@ def enemywalk():
         for list in out:
             writer.writerow(list)
     updateenemyposition()
+    fieldupdate()
 
 def searchenemy():
     with open("field.csv") as f:
@@ -155,15 +192,6 @@ def shoot():
                 else:
                     print("Aww... you missed...")
             count += 1
-
-def setfield(filename):
-    with open (filename) as f:
-        reader = csv.reader(f)
-        for line in reader:
-            temp = ""
-            for index in line:
-                temp += index
-            print(temp)
 
 def enemydeath():
     out = []
@@ -208,7 +236,7 @@ def enemyunregister(num1,num2):
 def emptycheck():
     with open("enemy.csv") as f:
         if(os.stat("enemy.csv").st_size == 0):
-            spawnenemy("field.csv",["BR"])
+            spawnenemy("field.csv",["R"])
 
 def checkplayerdeath():
     with open("enemy.csv") as f:
@@ -221,7 +249,7 @@ def checkplayerdeath():
 
 fieldwrite("field.csv")
 setplayer("field.csv")
-spawnenemy("field.csv",["BR"])
+spawnenemy("field.csv",["R"])
 print("Welcome to one of the tower defense games of all time! To review the rules and controls, please type rules. To see how to read the enemy stats, type enemy. This game was designed around a big enough console screen, so if the field looks wacky, extend the console by a bit and then reload the program. If you understand the rules or want to skip them, just start playing. Also, type stop to stop the game. Remember that typing help at any time will cause this to be printed again!")
 setfield("field.csv")
 
@@ -266,6 +294,26 @@ while forever == True:
         enemywalk()
         turn += 1
     print("it is currently turn " + str(turn) + " out of 50.")
+    if(turn == 5):
+        spawnenemy("field.csv",["R"])
+    if(turn == 8):
+        spawnenemy("field.csv",["R"])
+    if(turn == 9):
+        spawnenemy("field.csv",["R"])
+    if(turn == 14):
+        spawnenemy("field.csv",["R"])
+    if(turn == 15):
+        spawnenemy("field.csv",["R"])
+    if(turn == 16):
+        spawnenemy("field.csv",["R"])
+    if(turn == 20):
+        spawnenemy("field.csv",["R"])
+    if(turn == 25):
+        spawnenemy("field.csv",["R"])
+    if(turn == 26):
+        spawnenemy("field.csv",["R"])
+    if(turn == 27):
+        spawnenemy("field.csv",["R"])
     if(turn == 50):
         forever = False
         print("You've survived the day... congratulations on finishing the demo! I was getting sick of looking at csv files also it's 3:00 AM, so that's it for now! cya!")
